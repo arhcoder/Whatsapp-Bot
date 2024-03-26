2aa86e44a6bc9f316baf4e659c87dfa29ef5f34b
Alejandro Ramos <arhcoder@gmail.com>
2024-03-25 21:42:48 -0600

    Whatsapp Bots Template to recieve and answer messages to deploy inmediatly for example in Glitch.com

--- actions.py
+++ actions.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 
 import requests
-import json
-
 from dotenv import load_dotenv
+import json
+import os
 
+# Anaiza el formato del mensaje:
 def obtener_texto(message):
-    # Anaiza el formato del mensaje:
     type_message = message.get("type", None)
     if type_message == "text":
         return message["text"]["body"]
@@ -23,13 +23,14 @@ def obtener_texto(message):
 # FUNCIÓN PARA ENVIAR MENSAJES:
 def enviar_mensaje(message_json):
     # Construye un formato para enviar el mensaje construyendo el json para el request:
-    TOKEN = load_dotenv("TOKEN")
-    ENDPOINT = load_dotenv("ENDPOINT")
+    load_dotenv()
+    TOKEN = os.getenv("TOKEN")
+    ENDPOINT = os.getenv("ENDPOINT")
+    # print(message_json)
 
     try:
         headers = {"Content-Type": "application/json",
                    "Authorization": "Bearer " + TOKEN}
-        
         response = requests.post(ENDPOINT, headers=headers, data=message_json)
 
         # Si se envía un error 200:
@@ -43,12 +44,22 @@ def enviar_mensaje(message_json):
 
 
 # CONSTRUYE UN JSON CON UN MENSAJE DE TEXTO:
-def formato_texto(number, text):
-    data = json.dumps({
-        "messaging_product": "whatsapp",    
+def formato_template(template_name, number, variables):
+    parameters = [{"type": "TEXT", "text": variable} for variable in variables]
+    message = {
+        "messaging_product": "whatsapp",
         "recipient_type": "individual",
         "to": number,
-        "type": "text",
-        "text": {"body": text}
-    })
-    return data
+        "type": "template",
+        "template": {
+            "name": template_name,
+            "language": {"code": "es_MX"},
+            "components": [
+                {
+                    "type": "BODY",
+                    "parameters": parameters
+                }
+            ]
+        }
+    }
+    return json.dumps(message)
--- app.py
+++ app.py
@@ -1,69 +1,88 @@
 # coding: utf-8
 
-from flask import Flask, request
-from actions import obtener_texto, enviar_mensaje, formato_texto
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+from actions import obtener_texto, enviar_mensaje, formato_template
+import os
 
-# Creamos un objeto de Flask:
+import random
+import datetime
+
+# Objeto de Flask con la aplicación:
 app = Flask(__name__)
 
-# Ruta defecto:
-@app.route("/", methods=["GET"])
-def hello():
-    return "Hello World :3"
+# Ruta de saludo:
+@app.route("/hola", methods=["GET"])
+def  bienvenido():
+    return "Hola mundo :3"
 
-# Ruta para los webhooks, en donde se recibirán y enviarán mensajes:
-# Esto lo que hará será crear un url en el que nuestro bot atenderá peticiones;
-# PARA RECIBIR MENSAJES:
-@app.route("/webhook", methods=["GET", "POST"])
-def webhook():
-
-    WEBHOOKTOKEN = load_dotenv("WEBHOOKTOKEN")
-    
-    # Si la plataforma está conectando con nuestro bot:
-    if request.method == "GET":
-
-        # Obtiene el token del formato en que lo envía META:
-        if (
-            request.args.get("hub.mode") == "subscribe" and
-            request.args.get("hub.verify_token") == WEBHOOKTOKEN
-        ):
-            return request.args.get("hub.challenge")
+# Esta ruta para webhooks con el método "GET" es el estándar de
+# META para registrarlo como una API legítima:
+@app.route("/webhook", methods=["GET"])
+def verificar_webhook():
+    try:
+        # El TOKEN que nosotros inventamos para comprobar la conexión a META;
+        # Además de un "challenge", que es un dato propio de META para verificación:
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+        
+        # Nuestro Token que creamos para verificar conexiones:
+        load_dotenv()
+        WEBHOOKTOKEN = os.getenv("WEBHOOKTOKEN")
+        
+        # Verificación de TOKEN:
+        if token == WEBHOOKTOKEN and challenge != None:
+            return challenge
         else:
-            return "WEBHOOK TOKEN incorrecto :c", 400
+            return "Conexión con META incorrecta :c", 403
+    except Exception as error:
+        return error, 403
+
+# Esta ruta para webhook con método POST es para recibir mensajes
+# de texto y contestar con respecto a estos:
+@app.route("/webhook", methods=["POST"])
+def recibir_mensajes():
+    try:
+        # Se obtiene el número de teléfono:
+        body = request.get_json()
+        entry = body["entry"][0]
+        changes = entry["changes"][0]
+        value = changes["value"]
+        message = value["messages"][0]
+        number = message["from"]
+
+        # Se recibe el mensaje y se extrae el texto:
+        text = obtener_texto(message)
+
+        # Se procesa el mensaje y se responde:
+        response = respuestas_bot(text, number)
+        return jsonify({"status": "success", "message": "Mensaje respondido :3", "response": response, "from": number, "text": text})
     
+    except Exception as error:
+        return jsonify({"status": "error", "message": "Mensaje no enviado", "error": str(error)})
 
-    # Si nuestro bot está conectando a la plataforma:
-    # PARA ENVIAR MENSAJES:
-    elif request.method == "POST":
-        try:
-            # Se obtiene el número de teléfono:
-            body = request.get_json()
-            entry = body["entry"][0]
-            changes = entry["changes"][0]
-            value = changes["value"]
-            message = value["messages"][0]
-            number = message["from"]
-
-            # Se recibe el mensaje y se extrae el texto:
-            text = obtener_texto(message)
-
-            # Se procesa el mensaje y se responde:
-            response = respuestas_bot(text, number)
-            return "Mensaje respondido :3"
-        
-        except Exception as error:
-            return "Mensaje no enviado " + str(error) + ":c"
-
-
+# Respuestas e interacciones de texto del bot:
 def respuestas_bot(text, number):
+
+    # Si el mensaje de texto contiene un "hola":
     if "hola" in text.lower():
-        return enviar_mensaje(formato_texto(number, "¡Hola! ¿Cómo estás?"))
-    elif "adiós" in text.lower():
-        return enviar_mensaje(formato_texto(number, "¡Hasta luego!"))
-    else:
-        return enviar_mensaje(formato_texto(number, "Saluditos :3"))
+        # Variables para un template llamado "saludo":
+        dado1 = str(random.randint(1, 6))
+        dado2 = str(random.randint(1, 6))
         
+        return enviar_mensaje(formato_template("saludo", number, [dado1, dado2]))
+    
+    # Si el mensaje de texto contiene un "hora":
+    elif "hora" in text.lower():
+        # Variables para un template llamado "hora":
+        fecha = datetime.datetime.now()
+        hora = fecha.hour
+        minutos = fecha.minute
+        return enviar_mensaje(formato_template("hora", number, [hora, minutos]))
+    
+    # Si no coincide con saludo ni hora, envía un mensaje predeterminado:
+    else:
+        return enviar_mensaje(formato_template("default", number, []))
 
 # Punto de ejecución:
 if __name__ == "__main__":
